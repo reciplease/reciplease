e9d1d578b76906b4476242ff8d7812a477729634
Rhys Saldanha <rhys.saldanha@gmail.com>
2019-05-19 23:03:47 +0100

    Add src files from reciplease-inventory
    * rest in peace

---
+++ src/test/java/com/reciplease/inventory/RecipleaseApplicationTest.java
@@ -0,0 +1,13 @@
+package com.reciplease.inventory;
+
+import com.reciplease.RecipleaseApplication;
+import org.junit.Test;
+
+public class RecipleaseApplicationTest {
+
+    @Test
+    public void shouldRunMainMethod() {
+        RecipleaseApplication.main(new String[]{});
+    }
+
+}
