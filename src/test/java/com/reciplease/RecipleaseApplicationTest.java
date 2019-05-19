4daf2a095f80bbb3628cee0c035b4d63900cc5f4
Rhys Saldanha <rhys.saldanha@gmail.com>
2019-05-19 23:09:51 +0100

    Fix test

--- src/test/java/com/reciplease/RecipleaseApplicationTest.java
+++ src/test/java/com/reciplease/RecipleaseApplicationTest.java
@@ -1,13 +1,16 @@
-package com.reciplease.inventory;
+package com.reciplease;
 
-import com.reciplease.RecipleaseApplication;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class RecipleaseApplicationTest {
 
     @Test
     public void shouldRunMainMethod() {
         RecipleaseApplication.main(new String[]{});
+        assertThat(true, is(true));
     }
 
 }
