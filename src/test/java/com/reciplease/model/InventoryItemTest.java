5efb2ddccdcd29be890b8846742cd4b064adc7f3
Rhys Saldanha <rhys.saldanha@gmail.com>
2019-05-20 23:00:26 +0100

    Add expiry date to inventory item

--- src/test/java/com/reciplease/model/InventoryItemTest.java
+++ src/test/java/com/reciplease/model/InventoryItemTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Calendar;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
@@ -12,6 +14,8 @@ import static org.junit.Assert.assertThat;
 public class InventoryItemTest {
     @Mock
     private Ingredient ingredient;
+    @Mock
+    private Calendar expiration;
 
     @Before
     public void setUp() {
@@ -20,7 +24,7 @@ public class InventoryItemTest {
 
     @Test
     public void shouldCreateInstance() {
-        InventoryItem inventoryItem = new InventoryItem("id", ingredient, 10);
+        InventoryItem inventoryItem = new InventoryItem("id", ingredient, 10, expiration);
 
         assertThat(inventoryItem, is(notNullValue()));
         assertThat(inventoryItem.getAmount(), is(10));
