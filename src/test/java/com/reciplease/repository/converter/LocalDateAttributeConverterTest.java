57720cb1d3f865b0ee323c89e6767c23c106c270
Rhys Saldanha <rhys.saldanha@gmail.com>
2019-05-21 23:57:45 +0100

    Add more LocalDate converter tests

--- src/test/java/com/reciplease/repository/converter/LocalDateAttributeConverterTest.java
+++ src/test/java/com/reciplease/repository/converter/LocalDateAttributeConverterTest.java
@@ -7,6 +7,7 @@ import java.sql.Date;
 import java.time.LocalDate;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class LocalDateAttributeConverterTest {
@@ -18,12 +19,32 @@ public class LocalDateAttributeConverterTest {
     }
 
     @Test
-    public void shouldConvertLocalDateToDate() {
+    public void shouldConvertLocalDate() {
         final LocalDate localDate = LocalDate.of(2019, 2, 1);
         final Date expected = Date.valueOf(localDate);
 
         final Date actual = localDateAttributeConverter.convertToDatabaseColumn(localDate);
 
         assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void shouldConvertNullLocalDate() {
+        assertThat(localDateAttributeConverter.convertToDatabaseColumn(null), is(nullValue()));
+    }
+
+    @Test
+    public void shouldConvertDate() {
+        final LocalDate expected = LocalDate.of(2019, 2, 1);
+        final Date date = Date.valueOf(expected);
+
+        final LocalDate actual = localDateAttributeConverter.convertToEntityAttribute(date);
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void shouldConvertNullDate() {
+        assertThat(localDateAttributeConverter.convertToEntityAttribute(null), is(nullValue()));
     }
 }
