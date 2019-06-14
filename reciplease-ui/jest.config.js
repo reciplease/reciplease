0343f4caebadb6ffd668c81490a91620e84d973d
Rhys Saldanha <rhys.saldanha@gmail.com>
2019-06-14 08:26:44 +0100

    Add ui tests to circle-ci build

---
+++ reciplease-ui/jest.config.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line no-lone-blocks
+{
+  // eslint-disable-next-line no-restricted-syntax,no-labels,no-unused-labels,no-unused-expressions
+  reporters: ['default', 'jest-junit'];
+}
