d5afeff6bb6e4b5fdb616139e14e3785d9094abc
Rhys Saldanha <rhys.saldanha@gmail.com>
2019-06-14 08:48:26 +0100

    Fix jest junit run

--- reciplease-ui/jest.config.js
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line no-lone-blocks
-{
-  // eslint-disable-next-line no-restricted-syntax,no-labels,no-unused-labels,no-unused-expressions
-  reporters: ['default', 'jest-junit'];
-}
