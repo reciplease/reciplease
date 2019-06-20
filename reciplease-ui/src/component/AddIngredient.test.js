195cc7e111c2b1c25f38bc94e1b5e5da11f2bfc5
Rhys Saldanha <rhys.saldanha@gmail.com>
2019-06-20 22:34:11 +0100

    Add measures to AddIngredient
    * driven from API

--- reciplease-ui/src/component/AddIngredient.test.js
+++ reciplease-ui/src/component/AddIngredient.test.js
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import { mount, shallow } from 'enzyme';
+import fetchMock from 'fetch-mock';
 import AddIngredient from './AddIngredient';
 
 describe('AddIngredient component', () => {
   let component;
   const submitMock = jest.fn();
+  fetchMock.mock('api/profile/ingredients', {alps: {descriptor: [{descriptor: [{doc: {value: "measure1, measure2"}}]}]}});
 
   beforeEach(() => {
     component = shallow(
@@ -15,11 +17,15 @@ describe('AddIngredient component', () => {
       />,
     );
   });
+
   it('should render successfully', () => {
     expect(component.exists()).toEqual(true);
   });
   it('should have name input', () => {
     expect(component.find('.ingredient-name-input').length).toEqual(1);
+  });
+  it('should have measure select', () => {
+    expect(component.find('.ingredient-measure-select').length).toEqual(1);
   });
   describe('Add ingredient button', () => {
     it('should exist', () => {
