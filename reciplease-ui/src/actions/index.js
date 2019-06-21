c3aeb705c4443f5c8e0432f12221a71ae4b868f7
Rhys Saldanha <rhys.saldanha@gmail.com>
2019-06-21 08:22:29 +0100

    Remove unused code

--- reciplease-ui/src/actions/index.js
+++ reciplease-ui/src/actions/index.js
@@ -1,12 +1,5 @@
 import types from '../constants/';
 
-let ingredientId = 0;
-
-const nextId = () => {
-  ingredientId += 1;
-  return ingredientId;
-};
-
 const actions = {
   submitIngredient(ingredient) {
     return {
