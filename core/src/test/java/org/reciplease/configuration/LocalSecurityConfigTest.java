880146ce554fa6dd887d6b918cc56088a0d278b8
Rhys Saldanha <rhys.saldanha@gmail.com>
2019-09-28 12:01:43 +0100

    Change Spring test runner

--- core/src/test/java/org/reciplease/configuration/LocalSecurityConfigTest.java
+++ core/src/test/java/org/reciplease/configuration/LocalSecurityConfigTest.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -18,7 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @WebAppConfiguration
 @Import(LocalSecurityConfig.class)
 @ActiveProfiles("local")
