4d5039d43ce7018763d210fb4b96557ebc3a8e5d
Rhys Saldanha <rhys.saldanha@gmail.com>
2020-04-06 22:41:54 +0100

    Create database and web modules

--- database/src/main/java/org/reciplease/repository/IngredientRepository.java
+++ database/src/main/java/org/reciplease/repository/IngredientRepository.java
@@ -1,10 +1,16 @@
 package org.reciplease.repository;
 
 import org.reciplease.model.Ingredient;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface IngredientRepository extends JpaRepository<Ingredient, String> {
     List<Ingredient> findByNameContains(String searchName);
+
+    @Override
+    default <S extends Ingredient> boolean exists(final Example<S> example) {
+        return false;
+    }
 }
