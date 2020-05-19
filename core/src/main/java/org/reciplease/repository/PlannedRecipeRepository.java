75801c73813689586aec4a195909cc2ed5017e0c
Rhys Saldanha <rhys.saldanha@gmail.com>
2020-05-20 00:42:53 +0100

    UUIDs are not strings

--- core/src/main/java/org/reciplease/repository/PlannedRecipeRepository.java
+++ core/src/main/java/org/reciplease/repository/PlannedRecipeRepository.java
@@ -5,7 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
-public interface PlannedRecipeRepository extends JpaRepository<PlannedRecipe, String> {
+public interface PlannedRecipeRepository extends JpaRepository<PlannedRecipe, UUID> {
     List<PlannedRecipe> findByDateIsBetween(LocalDate start, LocalDate end);
 }
