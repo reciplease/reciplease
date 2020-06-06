2649dad81fc2f38e36c8e073943ba82e745a6b4a
Rhys Saldanha <rhys.saldanha@gmail.com>
2020-06-06 21:11:50 +0100

    WIP

--- core/src/main/java/org/reciplease/model/InventoryItem.java
+++ core/src/main/java/org/reciplease/model/InventoryItem.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
@@ -12,7 +13,7 @@ import java.time.LocalDate;
 
 @Entity
 @NoArgsConstructor
-@RequiredArgsConstructor
+@SuperBuilder(toBuilder = true)
 @Getter
 @EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
 public class InventoryItem extends BaseEntity {
