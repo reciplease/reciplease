5d6a6dbbd14fd347ef91567019df7036b1b933cc
Rhys Saldanha <rhys.saldanha@gmail.com>
2020-08-06 21:32:21 +0100

    Remove header logic

--- web/src/test/java/org/reciplease/controller/IngredientControllerTest.java
+++ web/src/test/java/org/reciplease/controller/IngredientControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -30,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(IngredientController.class)
+@ActiveProfiles("local")
 public class IngredientControllerTest {
 
     private static final String API_INGREDIENTS = "/api/ingredients";
--- web/src/test/java/org/reciplease/controller/MeasureControllerTest.java
+++ web/src/test/java/org/reciplease/controller/MeasureControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.reciplease.model.Measure;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(MeasureController.class)
+@ActiveProfiles("local")
 public class MeasureControllerTest {
     @Autowired
     private MockMvc mockMvc;
