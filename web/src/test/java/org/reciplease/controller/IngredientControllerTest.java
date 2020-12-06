e6a492fbf7a42ce7bc267de007c3af29e7a06211
Rhys Saldanha <rhys.saldanha@gmail.com>
2020-12-06 14:31:15 +0000

    Tidy tests

--- web/src/test/java/org/reciplease/controller/IngredientControllerTest.java
+++ web/src/test/java/org/reciplease/controller/IngredientControllerTest.java
@@ -30,7 +30,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
 @WebMvcTest(IngredientController.class)
 @ActiveProfiles("local")
 public class IngredientControllerTest {
