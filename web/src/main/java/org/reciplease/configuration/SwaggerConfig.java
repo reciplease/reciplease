84786b19b632c1f58d3542100ad740ebd052e7c4
Rhys Saldanha <rhys.saldanha@gmail.com>
2020-12-21 16:53:08 +0000

    Fix deployed app swagger not using HTTP

--- web/src/main/java/org/reciplease/configuration/SwaggerConfig.java
+++ web/src/main/java/org/reciplease/configuration/SwaggerConfig.java
@@ -2,6 +2,7 @@ package org.reciplease.configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,8 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI springShopOpenAPI(@Value("${reciplease.version}") final String version) {
         return new OpenAPI()
+                .addServersItem(new Server().url("/")
+                        .description("Current server"))
                 .info(new Info().title("Reciplease API")
                         .description("Recipe management and more!")
                         .version(version));
