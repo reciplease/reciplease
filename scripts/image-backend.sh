83340ffb6e926c7004765bab4d39334cc9fceecf
Rhys Saldanha <rhys.saldanha@gmail.com>
2021-01-03 17:50:54 +0000

    Push built image to GCR if CI

--- scripts/image-backend.sh
+++ scripts/image-backend.sh
@@ -7,14 +7,34 @@ if [[ -z "${PROJECT_ID}" ]]; then
 	exit 1
 fi
 
-JAR_FILE="./dist/target/reciplease-dist.jar"
+if [[ -z "${JAR_FILE}" ]]; then
+	echo '${JAR_FILE} is not set.'
+	exit 1
+fi
+
 if [[ ! -f "${JAR_FILE}" ]]; then
 	echo "${JAR_FILE} does not exist."
 	exit 1
 fi
 
+echo "Building gcr.io/${PROJECT_ID}/dist:latest"
+
 # Use Docker build kit to fix multiple COPYs in the Dockerfile
 DOCKER_BUILDKIT=1
-docker build \
+
+${DOCKER} build \
 	--build-arg JAR_FILE="${JAR_FILE}" \
 	-t "gcr.io/${PROJECT_ID}/dist:latest" .
+
+if [[ "${CI}" = true ]]; then
+	if [[ -z "${DOCKER_TOKEN}" ]]; then
+    echo '${DOCKER_TOKEN} is not set.'
+    exit 1
+  fi
+
+  echo "${DOCKER_TOKEN}" | ${DOCKER} login -u _json_key --password-stdin https://gcr.io
+
+  ${DOCKER} push gcr.io/${PROJECT_ID}/dist
+fi
+
+echo
