adae84c898598442a22a7dc4fab730cf4b7198c3
Rhys Saldanha <rhys.saldanha@gmail.com>
2021-01-03 17:43:08 +0000

    Tidy release script

--- scripts/release-backend.sh
+++ scripts/release-backend.sh
@@ -2,26 +2,20 @@
 # file: release-backend.sh
 # description: Update version, create commit and tag release.
 
-CURRENT_VERSION=`mvn -q \
-    -Dexec.executable=echo \
-    -Dexec.args='${project.version}' \
-    --non-recursive \
-    exec:exec`
-echo "Current Version: ${CURRENT_VERSION}"
 echo "New Release Version: ${RECIPLEASE_VERSION}"
-echo
 
-echo "Bumping project version."
-mvn versions:set \
+${MVN} versions:set \
 	-DnewVersion=${RECIPLEASE_VERSION} \
 	-DgenerateBackupPoms=false
-echo
 
-COMMIT_MESSAGE="[skip ci] Release version ${RECIPLEASE_VERSION}"
-echo "Creating commit: ${COMMIT_MESSAGE}"
+if [[ "${CI}" = true ]]; then
+  COMMIT_MESSAGE="[skip ci] Release version ${RECIPLEASE_VERSION}"
+  echo "Creating commit: ${COMMIT_MESSAGE}"
 
-if [[ "${CI}"=="true" ]]; then
-	git commit -am "${COMMIT_MESSAGE}"
-	git tag "v${RECIPLEASE_VERSION}"
-	git push --tags
+	${GIT} commit -am "${COMMIT_MESSAGE}"
+	${GIT} tag "v${RECIPLEASE_VERSION}"
+	${GIT} push --tags
+	${GIT} push
 fi
+
+echo
