6fc6887ab862658989b7b0cbcd18ff5f967b33b8
Rhys Saldanha <rhys.saldanha@gmail.com>
2021-01-10 14:22:28 +0000

    Enable CORS, /api endpoints are permitted

--- web/src/main/java/org/reciplease/configuration/LocalSecurityConfig.java
+++ web/src/main/java/org/reciplease/configuration/LocalSecurityConfig.java
@@ -1,27 +1,28 @@
 package org.reciplease.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Collections;
 
 @Profile("local")
 @Configuration
 @EnableWebSecurity
-public class LocalSecurityConfig extends WebSecurityConfigurerAdapter {
+public class LocalSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     @Override
     protected void configure(final HttpSecurity security) throws Exception {
         security.httpBasic().disable()
-                .csrf().disable().cors().disable()
+                .csrf().disable().cors().and()
                 .headers().frameOptions().disable();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedMethods("*");
     }
 }
