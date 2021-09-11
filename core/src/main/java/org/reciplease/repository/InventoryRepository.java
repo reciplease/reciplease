7cd8ade2f7569c69d6c7ac29d05039e2a9e02416
Rhys Saldanha <rhys.saldanha@gmail.com>
2021-09-11 12:49:56 +0100

    Add inventory service methods to get expired and unexpired inventory items

--- core/src/main/java/org/reciplease/repository/InventoryRepository.java
+++ core/src/main/java/org/reciplease/repository/InventoryRepository.java
@@ -2,8 +2,15 @@ package org.reciplease.repository;
 
 import org.reciplease.model.InventoryItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface InventoryRepository extends JpaRepository<InventoryItem, UUID> {
+    List<InventoryItem> findByExpirationIsGreaterThanEqual(LocalDate now);
+
+    List<InventoryItem> findByExpirationIsBefore(LocalDate now);
 }
