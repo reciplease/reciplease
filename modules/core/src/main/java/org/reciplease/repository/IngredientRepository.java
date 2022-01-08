9cd7ca736799a534529e85638d1cf8b30b0f181d
Vikash Kothary <kothary.vikash@gmail.com>
2022-01-08 22:53:34 +0000

    feat: Add missing methods to repository interfaces

--- modules/core/src/main/java/org/reciplease/repository/IngredientRepository.java
+++ modules/core/src/main/java/org/reciplease/repository/IngredientRepository.java
@@ -3,8 +3,14 @@ package org.reciplease.repository;
 import org.reciplease.model.Ingredient;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 // extends JpaRepository<Ingredient, UUID>
 public interface IngredientRepository {
+    List<Ingredient> findAll();
+    Optional<Ingredient> findById(final UUID uuid);
+    Ingredient save(final Ingredient ingredient);
+    List<Ingredient> saveAll(final List<Ingredient> ingredients);
     List<Ingredient> findByNameContains(String searchName);
 }
--- modules/core/src/main/java/org/reciplease/repository/InventoryRepository.java
+++ modules/core/src/main/java/org/reciplease/repository/InventoryRepository.java
@@ -4,9 +4,14 @@ import org.reciplease.model.InventoryItem;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 // extends JpaRepository<InventoryItem, UUID>
 public interface InventoryRepository {
+    Optional<InventoryItem> findById(final UUID uuid);
+    InventoryItem save(final InventoryItem item);
+    List<InventoryItem> findAll();
     List<InventoryItem> findByExpirationIsGreaterThanEqual(LocalDate now);
     List<InventoryItem> findByExpirationIsBefore(LocalDate now);
 }
