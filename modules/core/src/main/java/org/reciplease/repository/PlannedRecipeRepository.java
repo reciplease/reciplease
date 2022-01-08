b0d16fb058c22a9f270642fa6a01b6537446303b
Vikash Kothary <kothary.vikash@gmail.com>
2022-01-08 22:31:02 +0000

    refactor: Remove JpaRepository dependencies in core repository interfaces

--- modules/core/src/main/java/org/reciplease/repository/PlannedRecipeRepository.java
+++ modules/core/src/main/java/org/reciplease/repository/PlannedRecipeRepository.java
@@ -1,12 +1,11 @@
 package org.reciplease.repository;
 
 import org.reciplease.model.PlannedRecipe;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.UUID;
 
-public interface PlannedRecipeRepository extends JpaRepository<PlannedRecipe, UUID> {
+// extends JpaRepository<PlannedRecipe, UUID>
+public interface PlannedRecipeRepository {
     List<PlannedRecipe> findByDateIsBetween(LocalDate start, LocalDate end);
 }
