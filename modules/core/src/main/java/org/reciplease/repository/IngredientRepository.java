a73cb51c6db75f421e0a8b4d06e9dcf93deae4d3
Vikash Kothary <kothary.vikash@gmail.com>
2022-01-08 22:31:02 +0000

    refactor: Remove JpaRepository dependencies in core repository interfaces

--- modules/core/src/main/java/org/reciplease/repository/IngredientRepository.java
+++ modules/core/src/main/java/org/reciplease/repository/IngredientRepository.java
@@ -1,11 +1,10 @@
 package org.reciplease.repository;
 
 import org.reciplease.model.Ingredient;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.UUID;
 
-public interface IngredientRepository extends JpaRepository<Ingredient, UUID> {
+// extends JpaRepository<Ingredient, UUID>
+public interface IngredientRepository {
     List<Ingredient> findByNameContains(String searchName);
 }
--- modules/core/src/main/java/org/reciplease/repository/InventoryRepository.java
+++ modules/core/src/main/java/org/reciplease/repository/InventoryRepository.java
@@ -1,14 +1,12 @@
 package org.reciplease.repository;
 
 import org.reciplease.model.InventoryItem;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.UUID;
 
-public interface InventoryRepository extends JpaRepository<InventoryItem, UUID> {
+// extends JpaRepository<InventoryItem, UUID>
+public interface InventoryRepository {
     List<InventoryItem> findByExpirationIsGreaterThanEqual(LocalDate now);
-
     List<InventoryItem> findByExpirationIsBefore(LocalDate now);
 }
