98775a1ca61ebcfcadde202d3010a9e772d184e3
Vikash Kothary <kothary.vikash@gmail.com>
2022-01-08 23:46:45 +0000

    feat: Add super builder which includes randomUUID method

--- modules/core/src/main/java/org/reciplease/model/BaseModel.java
+++ modules/core/src/main/java/org/reciplease/model/BaseModel.java
@@ -1,7 +1,22 @@
 package org.reciplease.model;
 
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
 import java.util.UUID;
 
+@Getter
+@SuperBuilder(toBuilder = true)
 public abstract class BaseModel {
     private UUID uuid;
+
+    public abstract static class BaseModelBuilder<C extends BaseModel, B extends BaseModel.BaseModelBuilder<C, B>> {
+        private UUID uuid;
+
+        // Used for testing only
+        public B randomUUID() {
+            this.uuid = UUID.randomUUID();
+            return this.self();
+        }
+    }
 }
