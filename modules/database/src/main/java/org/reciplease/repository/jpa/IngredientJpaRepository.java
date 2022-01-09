b11452bc8e4f8dfe58a8ee76924186429180a13a
Vikash Kothary <kothary.vikash@gmail.com>
2022-01-09 03:46:37 +0000

    feat: Implement database reporitories using JPA

--- modules/database/src/main/java/org/reciplease/repository/jpa/IngredientJpaRepository.java
+++ modules/database/src/main/java/org/reciplease/repository/jpa/IngredientJpaRepository.java
@@ -3,7 +3,9 @@ package org.reciplease.repository.jpa;
 import org.reciplease.model.IngredientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface IngredientJpaRepository extends JpaRepository<IngredientEntity, UUID> {
+    List<IngredientEntity> findByNameContains(String searchName);
 }
--- modules/database/src/main/java/org/reciplease/repository/jpa/PlannedRecipeJpaRepository.java
+++ modules/database/src/main/java/org/reciplease/repository/jpa/PlannedRecipeJpaRepository.java
@@ -3,7 +3,10 @@ package org.reciplease.repository.jpa;
 import org.reciplease.model.PlannedRecipeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface PlannedRecipeJpaRepository extends JpaRepository<PlannedRecipeEntity, UUID> {
+    List<PlannedRecipeEntity> findByDateIsBetween(LocalDate start, LocalDate end);
 }
