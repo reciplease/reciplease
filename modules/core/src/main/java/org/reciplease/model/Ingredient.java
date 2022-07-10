d7c0f56564abf653c0074395d0ede51465ee7ba4
Rhys Saldanha <rhys-saldanha@gmail.com>
2022-07-11 00:05:58 +0100

    Add missing lombok annotation

--- modules/core/src/main/java/org/reciplease/model/Ingredient.java
+++ modules/core/src/main/java/org/reciplease/model/Ingredient.java
@@ -1,5 +1,6 @@
 package org.reciplease.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Value;
 import lombok.experimental.SuperBuilder;
 
@@ -7,6 +8,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Value
+@EqualsAndHashCode(callSuper = true)
 @SuperBuilder(toBuilder = true)
 public class Ingredient extends Identifiable {
     @NotNull
