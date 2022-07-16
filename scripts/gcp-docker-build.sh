ccb6d30aeecda28f1fd1e4b050266dbd88bd4182
Rhys Saldanha <rhys.saldanha@gmail.com>
2022-07-16 11:04:12 +0100

    Fix image deploy

--- scripts/gcp-docker-build.sh
+++ scripts/gcp-docker-build.sh
@@ -3,7 +3,7 @@
 # description: Build Docker image.
 
 if [[ -z "${GCP_PROJECT_ID}" ]]; then
-  echo '${GCP_PROJECT_ID} is not set.'
+  echo 'GCP_PROJECT_ID is not set.'
   exit 1
 fi
 
@@ -16,6 +16,19 @@ if [[ ! -f "${JAR_FILE}" ]]; then
   exit 1
 fi
 
+if [[ "${CI}" == true ]]; then
+  if [[ -z "${GCP_DOCKER_TOKEN}" ]]; then
+    echo 'GCP_DOCKER_TOKEN is not set.'
+    exit 1
+  fi
+
+  # Login to GCP Docker Registry
+  GCP_DOCKER_REGISTRY=https://gcr.io
+  GCP_DOCKER_USERNAME=_json_key
+  echo "Logging into GCP Docker Registry: ${GCP_DOCKER_REGISTRY}"
+  echo "${GCP_DOCKER_TOKEN}" | ${DOCKER} login -u ${GCP_DOCKER_USERNAME} --password-stdin ${GCP_DOCKER_REGISTRY}
+fi
+
 echo "Building gcr.io/${GCP_PROJECT_ID}/dist:latest"
 
 # Use Docker build kit to fix multiple COPYs in the Dockerfile
@@ -23,26 +36,8 @@ export DOCKER_BUILDKIT=1
 
 # Build Docker image.
 ${DOCKER} build \
+  --push \
   --build-arg JAR_FILE="${JAR_FILE}" \
   -t "gcr.io/${GCP_PROJECT_ID}/dist:latest" .
-
-if [[ "${CI}" != true ]]; then
-  echo "Images can only be pushed in CI environments. STOPPING."
-  exit 0
-fi
-
-if [[ -z "${GCP_DOCKER_TOKEN}" ]]; then
-  echo '${GCP_DOCKER_TOKEN} is not set.'
-  exit 1
-fi
-
-# Login to GCP Docker Registry
-GCP_DOCKER_REGISTRY=https://gcr.io
-GCP_DOCKER_USERNAME=_json_key
-echo "Logging into GCP Docker Registry: ${GCP_DOCKER_REGISTRY}"
-echo "${GCP_DOCKER_TOKEN}" | ${DOCKER} login -u ${GCP_DOCKER_USERNAME} --password-stdin ${GCP_DOCKER_REGISTRY}
-
-# Push image to Docker registry.
-${DOCKER} push "gcr.io/${GCP_PROJECT_ID}/dist:latest"
 
 echo
