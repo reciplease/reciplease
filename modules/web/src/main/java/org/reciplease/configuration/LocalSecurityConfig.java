3ff2aedb509ba702d957b7e833a9bcf8334a97f4
Rhys Saldanha <rhys-saldanha@gmail.com>
2022-10-28 23:58:40 +0100

    Tidy imports

--- modules/web/src/main/java/org/reciplease/configuration/LocalSecurityConfig.java
+++ modules/web/src/main/java/org/reciplease/configuration/LocalSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
